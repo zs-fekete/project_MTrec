5dc08b48aaacd6956abac1a210430774b35e3faa
Zsofia Fekete <fzsofi9238@gmail.com>
2023-10-23 14:47:57 +0300

    now it works for human AND mouse

--- Script/mtrec_detector.py
+++ Script/mtrec_detector.py
@@ -14,12 +14,12 @@ parser.add_argument('-p', '--out_positive',
                     help = 'filename for positive hit output')
 
 #Optional
-parser.add_argument('--out_negative',
+parser.add_argument('--out_negative', #momentarily doesn't do anything
                     action = 'store_true',
                     help = 'output negative results, too')
 parser.add_argument('--minstretch', type = int, default = 2,
                     help = 'Minimum length of type X to score positively')
-parser.add_argument('--opposite', nargs = '*', type = int,
+parser.add_argument('--opposite', nargs = '*', type = str,
                     default = ['14189', '14366'],
                     help = 'SNPs called, but more prominent in default type')
 
@@ -43,19 +43,40 @@ read_info = [ (read.query_name,
 
 var = sq.get_variants(args.vcffile)
 
-#Primitive detection of possible recombination
-with open('negative.out', 'w') as neg:
-    with open(args.out_positive, 'w') as out:
-        for read in read_info:
-            ref_snps = sq.snp_coordinates(read, ref_exclude)
-            nonsnps = sq.nonsnp_coordinates(var, read, ref_snps)
-                
-            readdic = sq.opposite_snps(args.opposite,
-                                    sq.create_readdic(var, read, ref_snps, nonsnps))
+print(args.opposite)
+print(args.out_positive)
+print(args.bamfile)
+print(args.vcffile)
+print(args.minstretch)
+print(args.out_negative)
+
+complete_reads = collections.defaultdict(lambda : [])
+
+readnames = list(set([read[0] for read in read_info]))
+fullreaddic = {readname : [] for readname in readnames}
+
+for read in read_info:
+    ref_snps = sq.snp_coordinates(read, ref_exclude)
+    nonsnps = sq.nonsnp_coordinates(var, read, ref_snps)
+        
+    readdic = sq.opposite_snps(args.opposite,
+                            sq.create_readdic(var, read, ref_snps, nonsnps))
+
+    if len(fullreaddic[read[0]]) != 0:
+        fullreaddic[read[0]] = fullreaddic[read[0]] | readdic
+    else:
+        fullreaddic[read[0]] = readdic
+    #print(readdic)
+
+with open(args.out_positive, 'w') as out:
+    out.write(f'read\tcigar\tresult\n')
+    with open('negative.out', 'w') as neg:
+        for read, readdic in fullreaddic.items():
             ps_cigar = sq.read_summary(list(readdic.values()))
             res = sq.rec_possible(ps_cigar, args.minstretch)
-        
+            
             if res == 'True':
-                out.write(f'{read[0]}\t{read[1]}\t' + ''.join(ps_cigar) + f'\t{res}\n')                        
-            elif args.out_negative:
-                neg.write(f'{read[0]}\t{read[1]}\t' + ''.join(ps_cigar) + f'\t{res}\n')                        
+                out.write(f'{read}\t' + ''.join(ps_cigar) + f'\t{res}\n')
+            else:
+                neg.write(f'{read}\t' + ''.join(ps_cigar) + f'\t{res}\n')
+
